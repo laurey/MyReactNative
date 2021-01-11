272a062cb05c8017459321bcaed22fdae5c534f0
laureyap <laureyap@yahoo.com>
2021-01-11 23:33:56 +0800

    fix run ios multi produce ttf error

--- components/AboutScreen.js
+++ components/AboutScreen.js
@@ -1,8 +1,12 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, TextInput} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 
 export default class AboutScreen extends Component {
+  onChangeText = (txt) => {
+    console.log('text: ', txt);
+  };
+
   render() {
     return (
       <SafeAreaView>
@@ -10,6 +14,14 @@ export default class AboutScreen extends Component {
           <Text>About Tab</Text>
         </View>
         <Text>Hello {this.props.name}!</Text>
+        <View>
+          <TextInput
+            placeholder="Please Input"
+            style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+            onChangeText={(text) => this.onChangeText(text)}
+            // value={value}
+          />
+        </View>
       </SafeAreaView>
     );
   }
